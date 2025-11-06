0a41de17b93d9ede1e9b3a2ab6d7336662f0ccee
lucas <lucascosta120903@gmail.com>
2025-11-05 21:50:01 -0300

    ajustes

--- core/settings.py
+++ core/settings.py
@@ -122,11 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Arquivos enviados por usuários (uploads)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
@@ -150,3 +146,10 @@ AUTH_USER_MODEL = 'users.User'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+
+# Local onde os arquivos estáticos serão reunidos em produção
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Pasta de uploads (se usar)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
